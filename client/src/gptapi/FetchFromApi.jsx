6a916f466a2f6e55b1d24179ee33cba59c0c71c4
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-01-31 21:27:00 +0545

    for deployent

--- client/src/gptapi/FetchFromApi.jsx
+++ client/src/gptapi/FetchFromApi.jsx
@@ -1,6 +1,6 @@
 export const fetchResponse = async (chat) => {
   try {
-    const response = await fetch("http://localhost:5000/", {
+    const response = await fetch("https://pkchat-gpt.onrender.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
